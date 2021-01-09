94260bfc15ff5ad827992288f74969340527ac5a
Luccas de Barros <luccas.barros@shareprime.com.br>
2021-01-09 16:52:53 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,9 @@
 import React, { useState } from 'react';
 
-function Contador() {
-    const [contador, setContador] = useState(0);
-    const acrescentar = () => {
-        setContador(contador + 1)
-    }
-
-    return (
-        <div>
-            {contador}
-            <button onClick={acrescentar}>Adicionar</button>
-        </div>
-
-    )
-}
-
 function Home() {
     return (
         <div>
-            <Contador />
+            Teste
         </div>
     );
 }
